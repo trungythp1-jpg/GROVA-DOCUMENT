741c643f0a26b1538793225466c6c0f6872d7dce
trungythp1-jpg <trungythp1@gmail.com>
2026-09-08 22:53:56 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ const APP_SHELL = [
   "./index.html",
   "./style.css?v=202",
   "./auth.css?v=202",
-  "./app.js?v=233",
+  "./app.js?v=236",
   "./auth.js?v=202",
   "./data/data.js?v=202",
   "./manifest.json?v=202",
