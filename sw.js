d0d8f7aab5b43f0b957c896856cd993b5c803fc5
trungythp1-jpg <trungythp1@gmail.com>
2026-09-08 13:06:38 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "grova-document-v14";
+const CACHE_NAME = "grova-document-v15";
 
 const APP_SHELL = [
   "./",
   "./index.html",
   "./style.css?v=202",
   "./auth.css?v=202",
-  "./app.js?v=227",
+  "./app.js?v=228",
   "./auth.js?v=202",
   "./data/data.js?v=202",
   "./manifest.json?v=202",
